cce25db8e2459fb05604ed1bed0fa784a9799bee
Peter Steinberger <steipete@gmail.com>
2025-05-18 09:02:03 +0100

    support re-export

---
+++ src/tools/index.ts
@@ -0,0 +1,4 @@
+export { handleList as list_notes } from './list.js';
+export { handleGet as get_note } from './get.js';
+export { handleSave as save_note } from './save.js';
+export { handleManage as manage_notes } from './manage.js'; 
